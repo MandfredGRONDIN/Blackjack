1c837ae4ed43fae7332d8df6760bc617bfd3015a
Mandfred GRONDIN <mandfred974@hotmail.fr>
2023-02-24 12:53:26 +0100

    Initialisation
    Initialisation du projet, création des dossier front et back, installation des dépendance, création des variables environnement.

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+
+const app = express();
+
+app.use((req, res, next) => {
+   // Accéder à notre API depuis n'importe quel origine
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   // Ajouter les headers mentionnés aux requêtes envoyées vers notre API
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+   );
+   // Envoyer des requêtes avec les méthodes mentionnées
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+   );
+   next();
+});
+
+// Transformer en json
+app.use(express.json());
+
+module.exports = app;
